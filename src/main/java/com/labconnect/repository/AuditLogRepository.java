3c66c224ba7be3c3cd0ba7e50b092b1fac3675dc
Shivani995 <Alugoju.Shivani@cognizant.com>
2026-02-24 15:19:32 +0530

    Add files via upload

---
+++ src/main/java/com/labconnect/repository/AuditLogRepository.java
@@ -0,0 +1,10 @@
+package com.labconnect.repository;
+
+import com.labconnect.models.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findByUser_UserId(Long userId);
+}
