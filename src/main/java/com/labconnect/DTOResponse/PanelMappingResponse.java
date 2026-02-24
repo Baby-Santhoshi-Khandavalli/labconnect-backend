aed050ce47297b7023209ae569c3a870a08ab11e
Shivani995 <Alugoju.Shivani@cognizant.com>
2026-02-24 11:25:03 +0530

    Add files via upload

---
+++ src/main/java/com/labconnect/DTOResponse/PanelMappingResponse.java
@@ -0,0 +1,14 @@
+package com.labconnect.DTOResponse;
+
+import lombok.Data;
+
+
+@Data
+public class PanelMappingResponse {
+    private Long mappingId;   // primary key of PanelMapping
+    private Long panelId;
+    private String panelName;
+    private Long testId;
+    private String testName;
+
+}
