3645f3fbfe51fc582c53b8098b17bae90e40374e
Shivani995 <Alugoju.Shivani@cognizant.com>
2026-02-24 15:18:46 +0530

    Add files via upload

---
+++ src/main/java/com/labconnect/models/AuditLog.java
@@ -0,0 +1,25 @@
+package com.labconnect.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name="audit_logs")
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long auditId;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+    private String action;
+    private String resource;
+    private LocalDateTime timestamp;
+
+    @Column(length=1000)
+    private String metadata;
+}
---
+++ src/main/java/com/labconnect/models/User.java
@@ -0,0 +1,36 @@
+package com.labconnect.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name="app_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long userId;
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String phone;
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.EAGER)
+    private List<AuditLog> auditLogs;
+
+//    @OneToMany(mappedBy = "user")
+//    private List<Notification> notifications;
+//
+//    @OneToMany(mappedBy = "clinician",fetch = FetchType.EAGER)
+//    private List<LabOrder> orders;
+    public enum Role{
+        Clinician,Technician,Pathologist,Manager,Admin
+    }
+}
