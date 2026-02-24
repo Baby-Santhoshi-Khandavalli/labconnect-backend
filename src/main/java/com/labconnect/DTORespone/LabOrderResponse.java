a87b203e553ff902682ae827088bf05b1d92e40c
Shivani995 <Alugoju.Shivani@cognizant.com>
2026-02-24 10:17:22 +0530

    Add files via upload
    "added"

---
+++ src/main/java/com/labconnect/DTORespone/LabOrderResponse.java
@@ -0,0 +1,19 @@
+package com.labconnect.DTOResponse;
+import com.labconnect.models.LabOrder;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+public class LabOrderResponse {
+    private Long orderId;
+    private Long patientId;
+    private Long clinicianId;
+    private LocalDateTime orderDate;
+    private LabOrder.Priority priority;
+    private LabOrder.OrderStatus status;
+    private Set<Long> testIds;
+}
---
+++ src/main/java/com/labconnect/DTORespone/NotificationResponse.java
@@ -0,0 +1,18 @@
+package com.labconnect.DTOResponse;
+
+import com.labconnect.models.Notification;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NotificationResponse {
+    private Long notificationId;
+    private Long userId;
+    private String message;
+    private Notification.Category category;
+    private Notification.Status status;
+    private LocalDateTime createdDate;
+}
